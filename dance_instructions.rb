974bc869ff2a831129a97fde96c9d7b30cf70d34
kwong991 <kgwong991@gmail.com>
2018-08-07 12:20:34 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,42 @@
+def lower_body
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
 
+def Base_Footwork
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  small_skip_back
+def skip_step
+  puts "Lower left foot to the ground"
+  puts "Bounce right foot back up slightly, kicking it a few inches back"
+  puts "Left, Right, Left and Left"
+def skip_step_right
+  puts "Lower right foot to the ground"
+  puts "Bounce right foot back up slightly, kicking it a few inches back"
+  puts "Right, Left, Right and Right"
+def bob_the_reins
+  puts "Holding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song."
+  bob_the_reins
+  bob_the_reins
+  bob_the_reins
+  bob_the_reins
+  bob_the_reins
+  bob_the_reins
+  bob_the_reins
+  bob_the_reins
+def Lasso
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your elbow straight left"
+  puts "Lift right arm up and point right elbow diagonally to the right"
+  puts "whip it in small circles"
+  Lasso
+  Lasso
+  Lasso
+  Lasso
+  Lasso
+  Lasso
+  Lasso
+  Lasso
+  def Lasso
 # Code your methods below
+end
